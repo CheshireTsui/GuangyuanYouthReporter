9cb87c91dd263acf26cd4854fce2d05129b7d570
CheshireTsui <857531567@qq.com>
2015-01-14 23:38:23 +0800

    Add the article list.

--- News/views.py
+++ News/views.py
@@ -10,14 +10,14 @@ class News():
         self.image = img
 
 
-def composeArticle(news_list):
+def composeArticle(news_list, n = 6):
     list_this = []
-    if len(news_list) < 6:
+    if len(news_list) < n:
         length = len(news_list) / 2
         if len(news_list) % 2 == 1:
             length += 1
     else:
-        length = 3
+        length = n/2
     for i in range(0,length):
         box = []
         box.append(news_list[i * 2])
@@ -56,7 +56,6 @@ def composeImage(raw_list):
         except Exception, e:
             box.append(None)
         list_this.append(box)
-    #return HttpResponse(list_this)
     return list_this
 
 
@@ -79,10 +78,82 @@ def Literature(request):
     idea_all = Column.objects.get(name="奇思妙想").article.all()
     imgs_list = []
     raw_list = Column.objects.get(name="书画廊").article.all()
-    for x in raw_list:
-        img = x.image.all()
-        imgs_list.append(img[0])
+    for i in raw_list:
+        img = i.image.all()
+        news = News(i,img[0])
+        imgs_list.append(news)
 
     shooting_list = composeImage(shooting_all)
     idea_list = composeArticle(idea_all)
     return render_to_response('literature.html',{'shooting':shooting_list, 'list':idea_list, 'img':imgs_list,})
+
+
+def Reporter(request):
+    news_all = Column.objects.get(name="小记者课堂").article.all()
+    imgs_all = Column.objects.get(name="小记者风采").article.all()
+    news_list = composeArticle(news_all)
+    imgs_list = composeImage(imgs_all)
+    try:
+        a1 = Article.objects.get(title="小记者报名")
+        a2 = Article.objects.get(title="我要投稿")
+    except Exception, e:
+        a1 = '#'
+        a2 = '#'  
+    return render_to_response('reporter.html',{'list':news_list, 'shooting':imgs_list, 'a1':a1, 'a2':a2,})
+
+
+def Content(request):
+    news_id = request.GET.get('news_id')
+    try:
+        news = Article.objects.get(id=news_id)
+        img = news.image.all()
+        img = img[0]
+    except Exception, e:
+        return HttpResponse("%s 请求的文章不存在！"%e)
+    return render_to_response('text.html',{'news':news, 'img':img,})
+
+
+def ArticleList(request):
+    news_column = request.GET.get('news_column')
+    if (not request.GET.get('news_page'))or(request.GET.get('news_page')==0):
+        news_page = 1
+    else:
+        news_page = int(request.GET.get('news_page'))
+    title = ''
+    if news_column:
+        if news_column == 'left-behind': 
+            news_all = Column.objects.get(name="留守之家").article.all()
+            title = "留守之家 <i class=\"fa fa-slideshare\"></i>"
+        elif news_column == 'news': 
+            news_all = Column.objects.get(name="校园新闻").article.all()
+            title = "校园新闻 <i class=\"fa fa-slideshare\"></i>"
+        elif news_column == 'shooting': 
+            news_all = Column.objects.get(name="我爱拍客").article.all()
+            title = "我爱拍客 <i class=\"fa fa-camera\"></i>"
+        elif news_column == 'imagine': 
+            news_all = Column.objects.get(name="奇思妙想").article.all()
+            title = "奇思妙想 <i class=\"fa fa-lightbulb-o\"></i>"
+        elif news_column == 'class': 
+            news_all = Column.objects.get(name="小记者课堂").article.all()
+            title = "小记者课堂 <i class=\"fa fa-key\"></i>"
+        elif news_column == 'reporter': 
+            news_all = Column.objects.get(name="小记者风采").article.all()
+            title = "小记者风采 <i class=\"fa fa-file-image-o\"></i>"
+        else: 
+            news_all = Article.objects.all()
+            title = "全部新闻 <i class=\"fa fa-university\"></i>"
+    else:
+        news_all = Article.objects.all()
+        title = "全部新闻 <i class=\"fa fa-university\"></i>"
+
+    cnt = len(news_all)/10
+    if len(news_all)%10!=0: cnt += 1
+    cnt = range(1,cnt+1)
+
+    try:
+        news_all = news_all[(news_page-1)*10:news_page*10]
+    except Exception, e:
+        news_all = news_all[(news_page-1)*10:]
+    news_list = composeArticle(news_all,10)
+
+    return render_to_response('article-list.html',{'title':title,'list':news_list, 'cnt':cnt, 'news_column':news_column, 'former':news_page-1,'later':news_page+1,})
